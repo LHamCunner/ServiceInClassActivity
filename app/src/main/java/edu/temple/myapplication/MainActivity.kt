bc8395affc9c398f0c1868c19b8bf0dbd0d05cd7
Liam Cunningham <tuo20284@temple.edu>
2024-04-16 11:44:48 -0400

    Added Menu Items to act the same as buttons

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -8,6 +8,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -52,6 +54,23 @@ class MainActivity : AppCompatActivity() {
             if(isConnected) timerBinder.stop()
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_Start ->  if(isConnected) timerBinder.start(100, handler)
+            R.id.activity_Pause -> if(isConnected) timerBinder.pause()
+            R.id.activity_Stop -> if(isConnected) timerBinder.stop()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
     override fun onDestroy(){
         super.onDestroy()
